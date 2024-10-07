f234b7418793789c89bf712ac56fb4b9b633b991
maghfoor-dev <maghfoor.dev@gmail.com>
2024-10-07 13:49:43 +0100

    added formatter for markdown

--- nvim/init.lua
+++ nvim/init.lua
@@ -706,6 +706,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        markdown = { 'markdownlint' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
